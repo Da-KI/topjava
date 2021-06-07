cd64762962417f67b46c322ecc27f0a9a34a07c6
Da-KI <84732254+Da-KI@users.noreply.github.com>
2021-06-07 23:32:17 +0500

    Apply 1_0_rename.patch

---
+++ src/main/java/ru/javawebinar/topjava/web/JSP.java
@@ -0,0 +1,31 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.UserMeal;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class JSP extends HttpServlet {
+    private static final Logger log = getLogger(JSP.class);
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        final int caloriesPerDay = 2000;
+
+
+//        request.getRequestDispatcher("/users.jsp").forward(request, response);
+        response.sendRedirect("meals.jsp");
+    }
+}
