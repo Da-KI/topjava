22a8b7e256bc968cb9c6fdf0677e109ee564c203
Da-KI <84732254+Da-KI@users.noreply.github.com>
2021-07-07 21:44:09 +0500

    HW05 tests separated for 3

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTestDatajpa.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTestDatajpa.java
@@ -3,5 +3,5 @@ package ru.javawebinar.topjava.service;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("datajpa")
-public class DatajpaMealServiceTest extends MealServiceTest {
+public class MealServiceTestDatajpa extends MealServiceTest {
 }
